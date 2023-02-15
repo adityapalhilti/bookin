7d4f36bfe9e7e7096d2294555fee73a7cd68a3c7
adityapalhilti <PALADI@HILTI.com>
2023-02-15 14:42:56 +0530

    cloud connection

--- src/main/java/com/bookin/bookin/controller/JwtController.java
+++ src/main/java/com/bookin/bookin/controller/JwtController.java
@@ -26,8 +26,6 @@ public class JwtController {
 
     @Autowired
     private JwtUtil jwtUtil;
-    @Autowired
-    private UserServiceImpl userServiceImpl;
 
     @Autowired
     private Producer producer;
